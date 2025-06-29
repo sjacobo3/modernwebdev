a8b96fcedb898f7cbfb2a7e836f637da6e9b8a15
Samara Jacobo <156864735+sjacobo3@users.noreply.github.com>
2025-06-29 13:58:22 -0400

    Update environments.js

--- src/environments.js
+++ src/environments.js
@@ -1,3 +1,5 @@
-VZTYzcR0ENpkBA4DQaM1Pl4tMTCIaEy6Xr1x6QBN - app id 
-s1vHq8YPryHPL80IiIpFUO9ovk90PFQTd7bclTvY - java script key 
-
+module.exports = {
+  APPLICATION_ID: "VZTYzcR0ENpkBA4DQaM1Pl4tMTCIaEy6Xr1x6QBN",
+  JAVASCRIPT_KEY: "s1vHq8YPryHPL80IiIpFUO9ovk90PFQTd7bclTvY",
+  SERVER_URL: "https://parseapi.back4app.com",
+};

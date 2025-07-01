8d35a54e8a3ad2d1102a8e9e5bcf0936388ebb0f
maiyenero <156826602+maiyenero@users.noreply.github.com>
2025-07-01 19:44:34 +0000

    nav bar

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,13 +2,15 @@ import './Home.css'
 function Home() {
     return (
         <div class="container">
-            <h1>Grade Expectations (Pick My Professor?)</h1>
-            <p style={{ fontSize: "16px", maxWidth: "600px", margin: "auto" }}>
+            <h1>Pick My Professor?</h1>
+            <p style={{ fontSize: "21px", maxWidth: "600px", margin: "auto" }}>
         Help fellow students choose their courses wisely!
-      </p>
+      </p> 
+      <br/>
             <div class="dome"></div>
         </div>
     );
 };
+
 
 export default Home;

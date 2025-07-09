1af2eea78e31cda1b7692c22d8283a0a283807ee
Samara Jacobo <sjacobo3@nd.edu>
2025-07-08 20:44:54 -0400

    added user logout

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -1,14 +1,19 @@
 import "../styles.css";
 
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
+import ProtectedRoute from "../Services/ProtectedRoute";
 import Navigation from "./Navigation/Navigation";
 import Home from "./Home/Home";
 import AuthRegister from "./Auth/AuthRegister";
 import AuthLogin from "./Auth/AuthLogin";
 import ReviewMain from "./Reviews/ReviewMain";
-import ProtectedRoute from "../Services/ProtectedRoute";
 
 const Components = () => {
   return (
@@ -18,7 +23,10 @@ const Components = () => {
         <Route path="/" element={<Home />} />
         <Route path="/auth/register" element={<AuthRegister />} />
         <Route path="/auth/login" element={<AuthLogin />} />
-        <Route path="/reviews" element={<ProtectedRoute element={ReviewMain} />} />
+        <Route
+          path="/reviews"
+          element={<ProtectedRoute element={ReviewMain} />}
+        />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>

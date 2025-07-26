b6d42915eaccc6e7f038a6c0339688d21bdfd5b0
maiyenero <156826602+maiyenero@users.noreply.github.com>
2025-07-26 21:44:05 +0000

    banner

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,22 @@
-import { Container, Typography, Paper } from "@mui/material";
+import { Container, Typography, Paper, Box} from "@mui/material";
+import NDImage from "../../assets/NotreDame.jpg";
+
 
 function Home() {
   return (
-    <Container maxWidth="md" sx={{ my: 8 }}>
+    //add ND banner 
+    <Container maxWidth="md" sx={{ my: 8 }}> 
       <Paper sx={{ padding: 4, backgroundColor: "white" }}>
+        <Box mb={4}>
+        <img
+        src={NDImage}
+        alt="Notre Dame"
+        style={{ width: "100%", height: "250px", objectFit: "cover", borderRadius: "12px" }}
+        />
+      </Box>
         <Typography variant="h1" mb={4}>
           Help fellow students choose their courses wisely!
         </Typography>
-
         <Typography variant="body1">
           Inspired by popular tools like Rate My Professors, we created a single
           platform where you can plan your academic journey in one convenient
